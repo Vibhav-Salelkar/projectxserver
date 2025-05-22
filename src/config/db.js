322ac0d1120184feb6fdbbfb583fb95b2afe4b88
Vibhav Sinai Salelkar <52214659+Vibhav-Salelkar@users.noreply.github.com>
2025-05-22 23:56:32 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    await mongoose.connect("");
+    await mongoose.connect("mongodb+srv://projectx:projectx321@projectx.3ipk1az.mongodb.net/");
 }
 
 module.exports = { connectDB };
